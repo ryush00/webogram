ca6dc8b93933207ab55cc1a976e2d8110c904b52
ryush00 <ryush00@gmail.com>
2014-12-25 20:42:52 +0900

    Revert "Revert "Adding ko-kr language in config.js""
    This reverts commit 687b1c7fd0c70603df8f6a1fcc5ce251ff298035.

--- app/js/lib/config.js
+++ app/js/lib/config.js
@@ -55,6 +55,7 @@ Config.I18n = {
     ,"it-it"
     ,"nl-nl"
     ,"pt-br"
+    ,"ko-kr"
     // ,"ru-ru"
   ], // To be copied to package.json
   languages: {
@@ -65,6 +66,7 @@ Config.I18n = {
     'ru-ru': 'Русский',
     'nl-nl': 'Nederlands',
     'pt-br': 'Português (Brazil)'
+    'ko-kr': '한국어'
   },
   aliases: {
     'en': 'en-us',
@@ -73,6 +75,7 @@ Config.I18n = {
     'it': 'it-it',
     'ru': 'ru-ru',
     'nl': 'nl-nl'
+    'ko': 'ko-kr'
   },
   messages: {},
   fallback_messages: {}
